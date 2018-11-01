12ad71d3e6bb70cfdc3579dcd63c306cc16f538b
pulkitG12 <44294579+pulkitG12@users.noreply.github.com>
2018-11-01 23:41:23 +0530

    Add files via upload

---
+++ bluetooth_connect.ino
@@ -0,0 +1,41 @@
+String readString;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(12,OUTPUT);
+pinMode(11,OUTPUT);
+}
+void loop()
+{
+  while(Serial.available())
+  {
+    delay(3);
+    char c = Serial.read();
+    readString += c;
+  }
+  if(readString.length() >0)
+  {
+    Serial.println(readString);
+    if(readString == "*where is my key#")
+   {
+    digitalWrite(12,HIGH);
+    digitalWrite(11,HIGH);
+  }
+    if(readString == "*where is my keychain#")
+   {
+    digitalWrite(12,HIGH);
+    digitalWrite(11,HIGH);
+  }
+   if(readString == "*find my key#")
+   {
+    digitalWrite(12,HIGH);
+    digitalWrite(11,HIGH);
+  }
+    else if(readString == "*found it#")
+   {
+    digitalWrite(12,LOW);
+    digitalWrite(11,LOW);
+  }
+  readString="";
+  }
+}
---
+++ wifi_connect.ino
@@ -0,0 +1,20 @@
+#define BLYNK_PRINT Serial   
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+
+char auth[] = "3fee3fdc72134e48ac4a78a86fb60dfc";
+
+char ssid[] = "pulkit";
+char pass[] = "12345677";
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
